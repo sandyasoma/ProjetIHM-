922ad6dcf9f4d2f8b66a89d57dbc645d59165462
Sandya <ssomasoundaram@gmail.com>
2017-06-22 19:35:48 +0200

    recherche de n'importe quel article

--- Code_QuickPurchase/fResearch.vb
+++ Code_QuickPurchase/fResearch.vb
@@ -1,5 +1,6 @@
 ﻿Public Class fResearch
-
+    Dim priceArticle As Double
+    Dim price As Double
     Private Sub lblTitre2_Click(sender As Object, e As EventArgs) Handles lblTitre2.Click
 
     End Sub
@@ -12,11 +13,24 @@
     Private Sub txtSearch_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtSearch.KeyPress
         If (e.KeyChar = ChrW(13)) Then
             EventValidatedSub()
+
         End If
     End Sub
 
     Private Sub EventValidatedSub()
         'Ta condition
+        lblquantity.Visible = True
+        txtquan.Visible = True
+        btnAdd.Enabled = True
+        btnAdd.Visible = True
+        If (txtSearch.Text <> "") Then
+            Dim random As New Random
+            lblArtticle.Text = txtSearch.Text
+            priceArticle = random.Next(0, 100)
+            lblPrice.Text = Convert.ToString(priceArticle)
+        Else
+            MsgBox("Please enter a field in the Search Field", MsgBoxStyle.Critical, "Warning !")
+        End If
     End Sub
 
     Private Sub txtSearch_TextChanged(sender As Object, e As EventArgs) Handles txtSearch.TextChanged
@@ -49,26 +63,36 @@
 
     Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
 
-        fSummer.lstArticle.Items.Add(lstBread.SelectedItem)
-        If (lstBread.SelectedItem = "Bread") Then
-            Dim prixBread As Double = 0
-            prixBread = (0.5 * Convert.ToDouble(txtquan.Text))
-            fSummer.calculerPrice(prixBread)
-            fSummer.lstprice.Items.Add(prixBread.ToString)
+        'Si la recherche = bread on affiche son résultat coresspondant
+        If (txtSearch.Text = "bread") Then
+            fSummer.lstArticle.Items.Add(lstBread.SelectedItem)
+            If (lstBread.SelectedItem = "Bread") Then
+                Dim prixBread As Double = 0
+                prixBread = (0.5 * Convert.ToDouble(txtquan.Text))
+                fSummer.calculerPrice(prixBread)
+                fSummer.lstprice.Items.Add(prixBread.ToString)
+            End If
+            If (lstBread.SelectedItem = "Soft bread") Then
+                Dim prixSoftBread As Double = 0
+                prixSoftBread = (0.5 * Convert.ToDouble(txtquan.Text))
+                fSummer.calculerPrice(prixSoftBread)
+                fSummer.lstprice.Items.Add(prixSoftBread.ToString)
+            End If
+            If (lstBread.SelectedItem = "Braided bread") Then
+                Dim prixBraidedBread As Double = 0
+                prixBraidedBread = (0.5 * Convert.ToDouble(txtquan.Text))
+                fSummer.calculerPrice(prixBraidedBread)
+                fSummer.lstprice.Items.Add(prixBraidedBread.ToString)
+            End If
+            fSummer.Show()
+        Else
+            'Si on cherche n'importe quel article
+            fSummer.lstArticle.Items.Add(lblArtticle.Text)
+            price = (priceArticle * Convert.ToDouble(txtquan.Text))
+            fSummer.lstprice.Items.Add(price.ToString)
+            fSummer.calculerPrice(price)
         End If
-        If (lstBread.SelectedItem = "Soft bread") Then
-            Dim prixSoftBread As Double = 0
-            prixSoftBread = (0.5 * Convert.ToDouble(txtquan.Text))
-            fSummer.calculerPrice(prixSoftBread)
-            fSummer.lstprice.Items.Add(prixSoftBread.ToString)
-        End If
-        If (lstBread.SelectedItem = "Braided bread") Then
-            Dim prixBraidedBread As Double = 0
-            prixBraidedBread = (0.5 * Convert.ToDouble(txtquan.Text))
-            fSummer.calculerPrice(prixBraidedBread)
-            fSummer.lstprice.Items.Add(prixBraidedBread.ToString)
-        End If
-        fSummer.Show()
+
 
     End Sub
 
